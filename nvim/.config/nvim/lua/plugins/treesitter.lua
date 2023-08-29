fd2b43adbe35641aa7cfe2b19554ca7b2a621447
“axzilla” <hello@axeladrian.com>
2023-08-29 07:29:16 +0200

    Astro treesitter ensured

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     config = function()
         require('nvim-treesitter.configs').setup {
-            ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+            ensure_installed = { 'astro', 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
             highlight = { enable = true }
         }
     end
