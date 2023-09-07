bc414b62989bc6df01e7b41e8e7af0347e024686
“axzilla” <hello@axeladrian.com>
2023-09-07 11:58:02 +0200

    Add dap restart

--- nvim/.config/nvim/lua/plugins/debugger.lua
+++ nvim/.config/nvim/lua/plugins/debugger.lua
@@ -16,14 +16,15 @@ return {
         vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
         vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
         vim.keymap.set('n', '<F6>', dap.disconnect, { desc = 'Debug: Disconnect' })
+        vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+        vim.keymap.set('n', '<F8>', dap.restart, { desc = 'Debug: Restart' })
+
         vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
         vim.keymap.set('n', '<leader>B', function()
             dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
         end, { desc = 'Debug: Set Breakpoint' })
 
         dapui.setup {}
-
-        vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
 
         dap.listeners.after.event_initialized['dapui_config'] = dapui.open
         dap.listeners.before.event_terminated['dapui_config'] = dapui.close
