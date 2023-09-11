27a1c5891a7fd5176ba218ef3d58d7fc93b4f3eb
“axzilla” <hello@axeladrian.com>
2023-09-11 22:41:30 +0200

    Add telescope oldfiles binding

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -25,5 +25,6 @@ return {
         vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
         vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
         vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+        vim.keymap.set('n', '<leader>so', require('telescope.builtin').oldfiles, { desc = 'Lists previously open files' })
     end
 }
