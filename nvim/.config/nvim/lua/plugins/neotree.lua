780a295c01a8dc96b14af3746d710d69e9809703
“axzilla” <hello@axeladrian.com>
2023-09-27 23:02:15 +0200

    Add neotree

---
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,17 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                follow_current_file = { enabled = true },
+            },
+        })
+        vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>', { desc = 'Toggle NeoTree' })
+    end
+}
