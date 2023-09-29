0e11d1d74a338c603f8f9c67c423d9f68afe9256
“axzilla” <hello@axeladrian.com>
2023-09-29 13:33:11 +0200

    Change to gruvbox theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,20 @@
 return {
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-        require('onedark').setup {
-            style = 'darker'
-        }
-        require('onedark').load()
-    end
+    -- 'navarasu/onedark.nvim',
+    -- priority = 1000,
+    -- config = function()
+    --     require('onedark').setup {
+    --         style = 'darker'
+    --     }
+    --     require('onedark').load()
+    -- end
+
+    -- add gruvbox
+    { "ellisonleao/gruvbox.nvim" },
+    -- Configure LazyVim to load gruvbox
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "gruvbox",
+        },
+    },
 }
