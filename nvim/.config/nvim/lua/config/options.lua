ae7b426a08a93b8dd23da61ece68c618a0038f54
“axzilla” <hello@axeladrian.com>
2023-09-29 21:55:39 +0200

    Disable noice, install notify, change some options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,37 +1,32 @@
-vim.opt.guicursor = ""
-vim.opt.colorcolumn = "80"
-vim.o.termguicolors = true
-vim.opt.scrolloff = 8
+o = vim.opt
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+o.clipboard = 'unnamedplus' -- enable clipboard
+o.cmdheight = 0             -- hide command line unless needed
+o.colorcolumn = "80"        -- show color column at text width 80
+o.cursorline = true         -- highlight the text line of the cursor
+o.expandtab = true          -- enable the use of space in tab
+o.guicursor = ""            -- show thick cursor instead of '|'
+o.ignorecase = true         -- case insensitive searching
+o.mouse = "a"               -- enable mouse support
+o.number = true             -- show numberline
+o.relativenumber = true     -- show relative numberline
+o.scrolloff = 8             -- lines of context while scrolling
+o.shiftwidth = 2            -- number of space inserted for indentation
+o.signcolumn = 'yes'        -- show sign column
+o.smartcase = true          -- case sensitive searching
+o.tabstop = 2               -- number of space in a tab
+o.termguicolors = true      -- enable 24-bit RGB color in the TUI
+o.timeoutlen = 300          -- shorten key timeout length a little bit for which-key
+o.updatetime = 250          -- length of time to wait before triggering the plugin
+o.wrap = false              -- disable line wrap
+-- o.fillchars = { eob = " " } -- disable `~` on nonexistent lines // TODO: changes weird colors on indent-blankline plugin
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.wo.number = true
-vim.wo.relativenumber = true
-
-vim.o.clipboard = 'unnamedplus'
-
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
-vim.wo.signcolumn = 'yes'
-
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
--- don't cheat =)
-vim.opt.mouse = ""
 
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = '*',
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
 })
