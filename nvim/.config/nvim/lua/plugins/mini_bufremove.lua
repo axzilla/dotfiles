774e739f438d80aa62177c093b0b1d012b7b0043
“axzilla” <hello@axeladrian.com>
2023-10-01 00:04:16 +0200

    Add mini.bufremove

---
+++ nvim/.config/nvim/lua/plugins/mini_bufremove.lua
@@ -0,0 +1,12 @@
+-- because :bd/:bdelete doesn't work with bufferline without bugs!
+return {
+  'echasnovski/mini.bufremove',
+  version = '*',
+  config = function()
+    require('mini.bufremove').setup()
+    vim.keymap.set('n', '<leader>bd', function() require("mini.bufremove").delete(0, false) end,
+      { desc = "Delete Buffer" })
+    vim.keymap.set('n', '<leader>bD', function() require("mini.bufremove").delete(0, true) end,
+      { desc = "Delete Buffer (Force)" })
+  end
+}
