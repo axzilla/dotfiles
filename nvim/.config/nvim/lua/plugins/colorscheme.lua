a5458fa6c7ca00d917f99fb23dbb8592114a490b
“axzilla” <hello@axeladrian.com>
2023-10-03 19:20:50 +0200

    Add additional colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,24 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
+  -- "catppuccin/nvim",
+  -- name = "catppuccin",
+  -- priority = 1000,
+  -- config = function()
+  --   require("catppuccin").setup({
+  --     color_overrides = {
+  --       mocha = {
+  --         base = "#262626",
+  --         mantle = "#000000",
+  --         crust = "#000000",
+  --       },
+  --     },
+  --   })
+  --
+  --   vim.cmd.colorscheme "catppuccin"
+  -- end
+  "rebelot/kanagawa.nvim",
   config = function()
-    require("catppuccin").setup({
-      color_overrides = {
-        mocha = {
-          base = "#262626",
-          mantle = "#000000",
-          crust = "#000000",
-        },
-      },
-    })
-
-    vim.cmd.colorscheme "catppuccin"
+    -- vim.cmd("colorscheme kanagawa-wave")
+    vim.cmd("colorscheme kanagawa-dragon")
+    -- vim.cmd("colorscheme kanagawa-lotus")
   end
 }
