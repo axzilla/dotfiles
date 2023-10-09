15784df7e7bb0b512004417ab4561344b626d442
“axzilla” <hello@axeladrian.com>
2023-10-09 15:27:55 +0200

    Update indent blankline

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,8 +1,17 @@
+-- return {
+--   'lukas-reineke/indent-blankline.nvim',
+--   config = function()
+--     require('indent_blankline').setup {
+--       show_current_context = true
+--     }
+--   end
+-- }
+
 return {
-  'lukas-reineke/indent-blankline.nvim',
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {},
   config = function()
-    require('indent_blankline').setup {
-      show_current_context = true
-    }
+    require("ibl").setup()
   end
 }
--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -1,11 +1,6 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
   config = function()
     vim.keymap.set('n', '<leader>fc', '<cmd>TodoTelescope<cr>', { desc = 'Find comments' })
   end
