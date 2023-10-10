5f49cf9ed29338ead53bc89c3beb87d2d3be61ba
“axzilla” <hello@axeladrian.com>
2023-10-10 06:43:47 +0200

    Change theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,24 +1,36 @@
+-- return {
+--   -- "catppuccin/nvim",
+--   -- name = "catppuccin",
+--   -- priority = 1000,
+--   -- config = function()
+--   --   require("catppuccin").setup({
+--   --     color_overrides = {
+--   --       mocha = {
+--   --         base = "#262626",
+--   --         mantle = "#000000",
+--   --         crust = "#000000",
+--   --       },
+--   --     },
+--   --   })
+--   --
+--   --   vim.cmd.colorscheme "catppuccin"
+--   -- end
+--   "rebelot/kanagawa.nvim",
+--   config = function()
+--     -- vim.cmd("colorscheme kanagawa-wave")
+--     vim.cmd("colorscheme kanagawa-dragon")
+--     -- vim.cmd("colorscheme kanagawa-lotus")
+--   end
+-- }
+
 return {
-  -- "catppuccin/nvim",
-  -- name = "catppuccin",
-  -- priority = 1000,
-  -- config = function()
-  --   require("catppuccin").setup({
-  --     color_overrides = {
-  --       mocha = {
-  --         base = "#262626",
-  --         mantle = "#000000",
-  --         crust = "#000000",
-  --       },
-  --     },
-  --   })
-  --
-  --   vim.cmd.colorscheme "catppuccin"
-  -- end
-  "rebelot/kanagawa.nvim",
+  "nyoom-engineering/oxocarbon.nvim",
   config = function()
-    -- vim.cmd("colorscheme kanagawa-wave")
-    vim.cmd("colorscheme kanagawa-dragon")
-    -- vim.cmd("colorscheme kanagawa-lotus")
+    vim.opt.background = "dark" -- set this to dark or light
+    vim.cmd.colorscheme "oxocarbon"
   end
+  -- Add in any other configuration;
+  --   event = foo,
+  --   config = bar
+  --   end,
 }
