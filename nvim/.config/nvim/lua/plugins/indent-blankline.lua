7ed983070fe5ca9251a2757029c99c73d5553e24
“axzilla” <hello@axeladrian.com>
2023-10-17 19:55:22 +0200

    Update

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,16 +1,6 @@
--- return {
---   'lukas-reineke/indent-blankline.nvim',
---   config = function()
---     require('indent_blankline').setup {
---       show_current_context = true
---     }
---   end
--- }
-
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
-  opts = {},
   config = function()
     require("ibl").setup()
   end
