2908c3068c7e5e8c129873e5b1f4afb9c64f7f3a
“axzilla” <hello@axeladrian.com>
2023-10-26 19:11:05 +0200

    Add colorscheme and Go debugger

--- nvim/.config/AstroNvim/lua/user/init.lua
+++ nvim/.config/AstroNvim/lua/user/init.lua
@@ -4,6 +4,9 @@ return {
     { import = "astrocommunity.pack.svelte" },
     { import = "astrocommunity.pack.typescript" },
     { import = "astrocommunity.pack.lua" },
+    { import = "astrocommunity.pack.go" },
     { import = "astrocommunity.colorscheme.oxocarbon-nvim" },
+    { import = "astrocommunity.colorscheme.kanagawa-nvim" },
   },
+  colorscheme = "kanagawa-dragon",
 }
