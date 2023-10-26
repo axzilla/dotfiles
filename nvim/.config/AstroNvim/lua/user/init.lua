bb9b6cc69d34c2bc93defb2cf1cacc24c01acf6a
“axzilla” <hello@axeladrian.com>
2023-10-26 16:32:58 +0200

    Bring the AstroNvim user folder into git

---
+++ nvim/.config/AstroNvim/lua/user/init.lua
@@ -0,0 +1,9 @@
+return {
+  plugins = {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.pack.svelte" },
+    { import = "astrocommunity.pack.typescript" },
+    { import = "astrocommunity.pack.lua" },
+    { import = "astrocommunity.colorscheme.oxocarbon-nvim" },
+  },
+}
