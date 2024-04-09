71a672aa8e0e28bdfe70fae1793a8a27466fb877
“axzilla” <hello@axeladrian.com>
2024-04-09 23:26:04 +0200

    Cleanup colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,34 +2,5 @@ return {
   "rebelot/kanagawa.nvim",
   config = function()
     vim.cmd("colorscheme kanagawa-dragon")
-    -- vim.cmd("colorscheme kanagawa-wave")
-    -- vim.cmd("colorscheme kanagawa-lotus")
   end
 }
-
--- return {
--- "catppuccin/nvim",
--- name = "catppuccin",
--- priority = 1000,
--- config = function()
---   require("catppuccin").setup({
---     color_overrides = {
---       mocha = {
---         base = "#262626",
---         mantle = "#000000",
---         crust = "#000000",
---       },
---     },
---   })
---
---   vim.cmd.colorscheme "catppuccin"
--- end
--- }
-
--- return {
---   "nyoom-engineering/oxocarbon.nvim",
---   config = function()
---     vim.opt.background = "dark" -- set this to dark or light
---     vim.cmd.colorscheme "oxocarbon"
---   end
--- }
