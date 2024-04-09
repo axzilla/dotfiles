d34a02f0ad252e2b964724ec55e781034a5f1595
“axzilla” <hello@axeladrian.com>
2024-04-09 23:26:30 +0200

    Better keymaps for comments

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -3,5 +3,9 @@ return {
   lazy = false,
   config = function()
     require('Comment').setup()
+    vim.keymap.set('n', '<Leader>/', function() require("Comment.api").toggle.linewise.count(vim.v.count1) end,
+      { desc = "Toggle comment line" })
+    vim.keymap.set('v', '<Leader>/', "<Esc><Cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+      { desc = "Toggle comment for selection" })
   end
 }
