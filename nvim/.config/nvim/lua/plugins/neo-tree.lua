853f3eb6e70c16f5cd74f17fe4c889d7235e42a0
“axzilla” <hello@axeladrian.com>
2024-04-10 15:13:22 +0200

    add mini.files, oil

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -20,13 +20,13 @@ return {
         follow_current_file = { enabled = true },
       },
     })
-    vim.keymap.set('n', '<Leader>e', '<Cmd>Neotree toggle<CR>', { desc = 'Toggle Explorer' })
+    vim.keymap.set('n', '<Leader>e', '<Cmd>Neotree toggle<CR>', { desc = 'Toggle Neotree' })
     vim.keymap.set('n', '<Leader>o', function()
       if vim.bo.filetype == "neo-tree" then
         vim.cmd.wincmd("p")
       else
         vim.cmd("Neotree focus")
       end
-    end, { desc = "Toggle Explorer Focus" })
+    end, { desc = "Toggle Neotree Focus" })
   end
 }
