e21fad483eda72f4c7877b88dc2b9fa367ca966d
“axzilla” <hello@axeladrian.com>
2024-04-11 14:02:58 +0200

    feat: add alpha dashboard

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -2,12 +2,13 @@ return {
   'rmagatti/auto-session',
   config = function()
     require("auto-session").setup({
+      auto_restore_enabled = false,
       log_level = "error",
       auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
       pre_save_cmds = { "Neotree close" },
-      vim.keymap.set("n", "<leader>fs", require("auto-session.session-lens").search_session, {
-        desc = "Find Session", noremap = true,
-      })
+      vim.keymap.set("n", "<leader>sd", "<cmd>:Autosession delete<cr>", { desc = "Delete Session" }),
+      vim.keymap.set("n", "<leader>sf", require("auto-session.session-lens").search_session,
+        { desc = "Find Session" })
     })
   end
 }
