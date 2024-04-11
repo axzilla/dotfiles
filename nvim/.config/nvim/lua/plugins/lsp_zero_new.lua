c4004d70c0e409a91409c9f19e58b9365bb93337
“axzilla” <hello@axeladrian.com>
2024-04-12 00:36:47 +0200

    change: rename some files

--- nvim/.config/nvim/lua/plugins/lsp_zero_new.lua
+++ nvim/.config/nvim/lua/plugins/lsp_zero_new.lua
@@ -10,6 +10,11 @@ return {
       vim.g.lsp_zero_extend_lspconfig = 0
     end,
   },
+  {
+    'williamboman/mason.nvim',
+    lazy = false,
+    config = true,
+  },
 
   -- Autocompletion
   {
@@ -48,25 +53,42 @@ return {
   -- LSP
   {
     'neovim/nvim-lspconfig',
-    cmd = 'LspInfo',
+    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
     event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
       { 'hrsh7th/cmp-nvim-lsp' },
+      { 'williamboman/mason-lspconfig.nvim' },
     },
     config = function()
       -- This is where all the LSP shenanigans will live
       local lsp_zero = require('lsp-zero')
       lsp_zero.extend_lspconfig()
 
+      --- if you want to know more about lsp-zero and mason.nvim
+      --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
       lsp_zero.on_attach(function(client, bufnr)
         -- see :help lsp-zero-keybindings
         -- to learn the available actions
         lsp_zero.default_keymaps({ buffer = bufnr })
       end)
 
-      -- (Optional) Configure lua language server for neovim
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
+      require('mason-lspconfig').setup({
+        ensure_installed = {},
+        handlers = {
+          -- this first function is the "default handler"
+          -- it applies to every language server without a "custom handler"
+          function(server_name)
+            require('lspconfig')[server_name].setup({})
+          end,
+
+          -- this is the "custom handler" for `lua_ls`
+          lua_ls = function()
+            -- (Optional) Configure lua language server for neovim
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+          end,
+        }
+      })
     end
   }
 }
