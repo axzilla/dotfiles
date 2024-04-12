0abe958a463992549605fdc6d673cb04fe4e8f37
“axzilla” <hello@axeladrian.com>
2024-04-12 10:51:14 +0200

    feat: remove lsp-zero, own lsp config WIP

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,46 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"neovim/nvim-lspconfig",
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-nvim-lsp",
+		"L3MON4D3/LuaSnip",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
+
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"tsserver",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"lua_ls",
+				"emmet_ls",
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettierd",
+				"stylua",
+				"eslint_d",
+			},
+		})
+	end,
+}
