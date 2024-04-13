d508449c7b143d906fad57b7f474dfae9d1500c9
“axzilla” <hello@axeladrian.com>
2024-04-13 20:11:50 +0200

    feat: gitsigns, remap keys

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -2,17 +2,27 @@ return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
 		require("gitsigns").setup({
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "-" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
+			-- signs = {
+			-- add = { text = "+" },
+			-- change = { text = "~" },
+			-- delete = { text = "-" },
+			-- topdelete = { text = "‾" },
+			-- changedelete = { text = "~" },
+			-- },
 		})
-		vim.keymap.set("n", "<leader>gp", require("gitsigns").prev_hunk, { desc = "Previous Hunk" })
-		vim.keymap.set("n", "<leader>gn", require("gitsigns").next_hunk, { desc = "Next Hunk" })
-		vim.keymap.set("n", "<leader>gh", require("gitsigns").preview_hunk, { desc = "Preview Hunk" })
-		vim.keymap.set("n", "<leader>gt", require("gitsigns").toggle_current_line_blame, { desc = "Toggle Line Blame" })
+		local keymap = vim.keymap
+		local gs = require("gitsigns")
+
+		keymap.set("n", "]g", gs.next_hunk, { desc = "Next Hunk" })
+		keymap.set("n", "[g", gs.prev_hunk, { desc = "Previous Hunk" })
+		keymap.set("n", "<Leader>gb", gs.blame_line, { desc = "View Blame" })
+		keymap.set("n", "<Leader>gB", gs.toggle_current_line_blame, { desc = "Toogle Blame" })
+		keymap.set("n", "<Leader>gp", gs.preview_hunk_inline, { desc = "Preview Hunk" })
+		keymap.set("n", "<Leader>gr", gs.reset_hunk, { desc = "Reset Hunk" })
+		keymap.set("n", "<Leader>gR", gs.reset_buffer, { desc = "Reset Buffer" })
+		keymap.set("n", "<Leader>gs", gs.stage_hunk, { desc = "Stage Hunk" })
+		keymap.set("n", "<Leader>gS", gs.stage_buffer, { desc = "Stage Buffer" })
+		keymap.set("n", "<Leader>gu", gs.undo_stage_hunk, { desc = "Unstage Hunk" })
+		keymap.set("n", "<Leader>gd", gs.diffthis, { desc = "View Diff" })
 	end,
 }
