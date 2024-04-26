f5b1f2e09a70d3a8aa5e2e3346e99972f2d94f9f
“axzilla” <hello@axeladrian.com>
2024-04-26 16:09:13 +0200

    feat: wezterm, update settings

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,17 +1,21 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
 config.font = wezterm.font("JetBrains Mono")
 config.hide_tab_bar_if_only_one_tab = true
--- You can specify some parameters to influence the font selection;
--- for example, this selects a Bold, Italic font variant.
--- config.font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = true })
--- For example, changing the color scheme:
--- config.color_scheme = "AdventureTime"
---
--- and finally, return the configuration to wezterm
+config.background = {
+	{
+		opacity = 0.1,
+		height = "Cover",
+		width = "Cover",
+		repeat_x = "NoRepeat",
+		repeat_y = "NoRepeat",
+		vertical_align = "Middle",
+		horizontal_align = "Center",
+		source = {
+			File = os.getenv("HOME") .. "/.wezterm-bg.jpeg",
+		},
+	},
+}
+
 return config
