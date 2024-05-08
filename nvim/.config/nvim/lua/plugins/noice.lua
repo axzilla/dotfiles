40eb48b42bb9a1904b5cd1d6848e695fae5bb8b3
“axzilla” <hello@axeladrian.com>
2024-05-08 21:05:29 +0200

    noice: change settings

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -8,7 +8,7 @@ return {
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
+		-- "rcarriga/nvim-notify",
 	},
 	config = function()
 		require("noice").setup({
@@ -22,14 +22,14 @@ return {
 			},
 			-- you can enable a preset for easier configuration
 			presets = {
-				-- bottom_search = true, -- use a classic bottom cmdline for search
+				bottom_search = true, -- use a classic bottom cmdline for search
 				command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
 			cmdline = {
-				-- view = "cmdline", -- classic cmdline instead of floating
+				view = "cmdline", -- classic cmdline instead of floating
 			},
 		})
 		vim.keymap.set("n", "<leader>fn", "<cmd>Noice telescope<cr> ", { desc = "Find Noice" })
