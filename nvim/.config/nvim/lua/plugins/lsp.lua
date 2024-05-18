4c18163418b368b6d88c89c4784e16ba66e48b70
“axzilla” <hello@axeladrian.com>
2024-05-18 18:50:28 +0200

    enhancement: restructure lsp and completion

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -8,19 +8,8 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 		"neovim/nvim-lspconfig",
-		{ "folke/neodev.nvim", opts = {} },
-		"hrsh7th/nvim-cmp",
 		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"onsails/lspkind.nvim",
-		{
-			"L3MON4D3/LuaSnip",
-			dependencies = {
-				"saadparwaiz1/cmp_luasnip",
-				"rafamadriz/friendly-snippets",
-			},
-		},
+		{ "folke/neodev.nvim", opts = {} },
 	},
 	config = function()
 		local mason = require("mason")
@@ -155,37 +144,6 @@ return {
 						},
 					})
 				end,
-				-- templ = function()
-				-- 	require("lspconfig").templ.setup({
-				-- 		cmd = { "templ", "lsp", "-http=localhost:7474", "-log=/Users/axeladrian/templ.log" },
-				-- 		filetypes = { "templ" },
-				-- 		root_dir = require("lspconfig.util").root_pattern("go.mod", ".git"),
-				-- 		capabilities = lsp_capabilities,
-				-- 		handlers = handlers,
-				-- 	})
-				-- end,
-				-- html = function()
-				-- 	require("lspconfig").html.setup({
-				-- 		-- on_attach = on_attach,
-				-- 		capabilities = lsp_capabilities,
-				-- 		filetypes = { "html", "templ" },
-				-- 	})
-				-- end,
-				-- htmx = function()
-				-- 	require("lspconfig").htmx.setup({
-				-- 		-- on_attach = on_attach,
-				-- 		capabilities = lsp_capabilities,
-				-- 		filetypes = { "html", "templ" },
-				-- 	})
-				-- end,
-				-- tailwindcss = function()
-				-- 	require("lspconfig").tailwindcss.setup({
-				-- 		-- on_attach = on_attach,
-				-- 		capabilities = lsp_capabilities,
-				-- 		filetypes = { "templ", "astro", "javascript", "typescript", "react" },
-				-- 		init_options = { userLanguages = { templ = "html" } },
-				-- 	})
-				-- end,
 			},
 		})
 
@@ -195,67 +153,6 @@ return {
 				"stylua",
 				"eslint_d",
 				"goimports",
-			},
-		})
-
-		local cmp = require("cmp")
-		local lspkind = require("lspkind")
-
-		cmp.setup({
-			sources = {
-				{ name = "copilot" },
-				{ name = "path" },
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "buffer" },
-			},
-			---@diagnostic disable-next-line: missing-fields
-			formatting = {
-				format = lspkind.cmp_format({
-					maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-					ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-					show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-					symbol_map = {
-						Copilot = icons.kinds.Copilot,
-					},
-					-- before = function(entry, vim_item) -- Optional // This shows the cmp source
-					-- 	vim_item.menu = ({
-					-- 		copilot = "[Copilot]",
-					-- 		path = "[Path]",
-					-- 		nvim_lsp = "[LSP]",
-					-- 		luasnip = "[LuaSnip]",
-					-- 		buffer = "[Buffer]",
-					-- 	})[entry.source.name]
-					-- 	return vim_item
-					-- end,
-				}),
-			},
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-			mapping = cmp.mapping.preset.insert({
-				-- Select next and previous completion item.
-				["<C-k>"] = cmp.mapping.select_prev_item(),
-				["<C-j>"] = cmp.mapping.select_next_item(),
-
-				-- Close completion window.
-				["<C-e>"] = cmp.mapping.abort(),
-
-				-- Scroll up and down the documentation window.
-				["<C-u>"] = cmp.mapping.scroll_docs(-4),
-				["<C-d>"] = cmp.mapping.scroll_docs(4),
-
-				-- Enter key confirms completion item.
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-
-				-- Ctrl + space triggers completion menu.
-				["<C-Space>"] = cmp.mapping.complete(),
-			}),
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
-				end,
 			},
 		})
 	end,
