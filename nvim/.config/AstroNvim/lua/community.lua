12d979cee634c4c5cfa485fa3d5d402e124b0450
“axzilla” <hello@axeladrian.com>
2024-05-18 08:30:10 +0200

    update astro

--- nvim/.config/AstroNvim/lua/community.lua
+++ nvim/.config/AstroNvim/lua/community.lua
@@ -9,5 +9,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.templ" },
   -- import/override with your plugins folder
 }
