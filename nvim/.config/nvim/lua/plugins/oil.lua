423047dcb8b2fde6fef96734481e5f2c1f0c5e00
“axzilla” <hello@axeladrian.com>
2024-05-19 21:32:17 +0200

    add oil trash

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -8,6 +8,7 @@ return {
 			view_options = {
 				show_hidden = true,
 			},
+			delete_to_trash = true,
 		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open Oil (Buffer)" })
 		vim.keymap.set("n", "eo", "<CMD>Oil<CR>", { desc = "Open Oil (Buffer)" })
