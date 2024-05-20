cc548e2288a2bbf46d1a4b49aed7727532f16d57
“axzilla” <hello@axeladrian.com>
2024-05-20 10:55:01 +0200

    use different gitsigns signs

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -2,27 +2,28 @@ return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
 		require("gitsigns").setup({
-			-- signs = {
-			-- add = { text = "+" },
-			-- change = { text = "~" },
-			-- delete = { text = "-" },
-			-- topdelete = { text = "‾" },
-			-- changedelete = { text = "~" },
-			-- },
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "-" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
 		})
+
 		local keymap = vim.keymap
 		local gs = require("gitsigns")
 
-		keymap.set("n", "]g", gs.next_hunk, { desc = "Next Hunk" })
-		keymap.set("n", "[g", gs.prev_hunk, { desc = "Previous Hunk" })
-		keymap.set("n", "<Leader>gb", gs.blame_line, { desc = "View Blame" })
-		keymap.set("n", "<Leader>gB", gs.toggle_current_line_blame, { desc = "Toogle Blame" })
-		keymap.set("n", "<Leader>gp", gs.preview_hunk_inline, { desc = "Preview Hunk" })
-		keymap.set("n", "<Leader>gr", gs.reset_hunk, { desc = "Reset Hunk" })
-		keymap.set("n", "<Leader>gR", gs.reset_buffer, { desc = "Reset Buffer" })
-		keymap.set("n", "<Leader>gs", gs.stage_hunk, { desc = "Stage Hunk" })
-		keymap.set("n", "<Leader>gS", gs.stage_buffer, { desc = "Stage Buffer" })
-		keymap.set("n", "<Leader>gu", gs.undo_stage_hunk, { desc = "Unstage Hunk" })
-		keymap.set("n", "<Leader>gd", gs.diffthis, { desc = "View Diff" })
+		keymap.set("n", "]g", gs.next_hunk, { desc = "Gitsigns Next Hunk" })
+		keymap.set("n", "[g", gs.prev_hunk, { desc = "Gitsigns Previous Hunk" })
+		keymap.set("n", "<Leader>ggb", gs.blame_line, { desc = "Gitsigns View Blame" })
+		keymap.set("n", "<Leader>ggB", gs.toggle_current_line_blame, { desc = "Gitsigns Toogle Blame" })
+		keymap.set("n", "<Leader>ggp", gs.preview_hunk_inline, { desc = "Gitsigns Preview Hunk" })
+		keymap.set("n", "<Leader>ggr", gs.reset_hunk, { desc = "Gitsigns Reset Hunk" })
+		keymap.set("n", "<Leader>ggR", gs.reset_buffer, { desc = "Gitsigns Reset Buffer" })
+		keymap.set("n", "<Leader>ggs", gs.stage_hunk, { desc = "Gitsigns Stage Hunk" })
+		keymap.set("n", "<Leader>ggS", gs.stage_buffer, { desc = "Gitsigns Stage Buffer" })
+		keymap.set("n", "<Leader>ggu", gs.undo_stage_hunk, { desc = "Gitsigns Unstage Hunk" })
+		keymap.set("n", "<Leader>ggd", gs.diffthis, { desc = "Gitsigns View Diff" })
 	end,
 }
