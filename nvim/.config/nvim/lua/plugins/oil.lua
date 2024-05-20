4ca155dd8f78ba8b7d5b3d4ac27e684f5cf84cab
“axzilla” <hello@axeladrian.com>
2024-05-20 15:18:33 +0200

    feat: neovim, remove oil comment

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,6 @@
 return {
 	"stevearc/oil.nvim",
 	opts = {},
-	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("oil").setup({
@@ -10,6 +9,7 @@ return {
 			},
 			delete_to_trash = true,
 		})
+
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open Oil (Buffer)" })
 		vim.keymap.set("n", "eo", "<CMD>Oil<CR>", { desc = "Open Oil (Buffer)" })
 	end,
