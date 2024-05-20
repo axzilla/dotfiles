07b5f31a8abcca4b77e8ebb5c996918f0cf14ad9
“axzilla” <hello@axeladrian.com>
2024-05-20 15:17:13 +0200

    feat: neovim, better mappings for diffview

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -1,68 +1,30 @@
 return {
 	"sindrets/diffview.nvim",
 	config = function()
+		local diffview = require("diffview")
 		local actions = require("diffview.actions")
 
-		require("diffview").setup({
+		diffview.setup({
 			key_bindings = {
 				file_panel = {
-					{
-						"n",
-						"<leader>e",
-						actions.toggle_files,
-						{ desc = "Toggle the file panel" },
-					},
-					{
-						"n",
-						"<leader>o",
-						actions.focus_files,
-						{ desc = "Bring focus to the file panel" },
-					},
+					{ "n", "<leader>e", actions.toggle_files, { desc = "toggle the file panel" } },
+					{ "n", "<leader>o", actions.focus_files, { desc = "bring focus to the file panel" } },
+					{ "n", "q", diffview.close, { desc = "close diffview" } },
+				},
+				file_history_panel = {
+					{ "n", "<leader>e", actions.toggle_files, { desc = "toggle the file panel" } },
+					{ "n", "<leader>o", actions.focus_files, { desc = "bring focus to the file panel" } },
+					{ "n", "q", diffview.close, { desc = "close diffview" } },
 				},
 				view = {
-					{
-						"n",
-						"<leader>e",
-						actions.toggle_files,
-						{ desc = "Toggle the file panel" },
-					},
-					{
-						"n",
-						"<leader>o",
-						actions.focus_files,
-						{ desc = "Bring focus to the file panel" },
-					},
+					{ "n", "<leader>e", actions.toggle_files, { desc = "Toggle the file panel" } },
+					{ "n", "<leader>o", actions.focus_files, { desc = "Bring focus to the file panel" } },
+					{ "n", "q", diffview.close, { desc = "Close Diffview" } },
 				},
 			},
 		})
 
-		local function toggle_diffview()
-			local view = require("diffview.lib").get_current_view()
-			if view then
-				vim.cmd("DiffviewClose")
-			else
-				vim.cmd("DiffviewOpen")
-			end
-		end
-
-		local function toggle_file_history()
-			local view = require("diffview.lib").get_current_view()
-			if view then
-				vim.cmd("DiffviewClose")
-			else
-				vim.cmd("DiffviewFileHistory")
-			end
-		end
-
-		vim.keymap.set(
-			"n",
-			"<leader>gdh",
-			toggle_file_history,
-			{ noremap = true, silent = true, desc = "Toggle File History" }
-		)
-
-		vim.keymap.set("n", "<leader>gdd", toggle_diffview, { noremap = true, silent = true, desc = "Toggle Diffview" })
-
-		-- vim.keymap.set("n", "<leader>gdd", "<cmd>DiffviewOpen<cr>", { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>gdh", "<cmd>DiffviewFileHistory<cr>", { desc = "Diffview File History" })
+		vim.keymap.set("n", "<leader>gdd", "<cmd>DiffviewOpen<cr>", { desc = "Diffview" })
 	end,
 }
