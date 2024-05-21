5adb98bf4a5b91fdba289b54a20485ebac964630
“axzilla” <hello@axeladrian.com>
2024-05-21 14:37:38 +0200

    feat: change wezterms line_height

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,5 +5,6 @@ config.native_macos_fullscreen_mode = false
 config.font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = true })
 config.hide_tab_bar_if_only_one_tab = true
 config.font_size = 14
+config.line_height = 1.2
 
 return config
