447f03fa79986c9cee1bc842f21615f02e3048dd
“axzilla” <hello@axeladrian.com>
2024-05-21 09:33:40 +0200

    feat: currently use own autotag fork because of Templ auto rename bug

--- nvim/.config/nvim/lua/plugins/nvim-ts-autotag.lua
+++ nvim/.config/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -1,6 +1,8 @@
 ---@diagnostic disable: missing-parameter
 return {
-	"windwp/nvim-ts-autotag",
+	"axzilla/nvim-ts-autotag",
+	branch = "fix/templ-auto-rename",
+	-- dir = "~/dev/nvim-ts-autotag",
 	config = function()
 		local ts_autotag = require("nvim-ts-autotag")
 		ts_autotag.setup()
