d7cefdcd0321915ec2d27ea2708d705ce8f04169
“axzilla” <hello@axeladrian.com>
2024-05-22 13:37:32 +0200

    feat: neovim, zen-mode improvements

--- nvim/.config/nvim/lua/plugins/zen-mode.lua
+++ nvim/.config/nvim/lua/plugins/zen-mode.lua
@@ -10,15 +10,24 @@ return {
 		on_open = function()
 			-- HACK: This is a hack to fix the issue with barbecue not being hidden everytime (just the first time)
 			require("barbecue.ui").toggle()
+			vim.notify("Zen Mode enabled", "info", { title = "Zen Mode" })
 		end,
 		on_close = function()
 			-- HACK: This is a hack to fix the issue with barbecue not being hidden everytime (just the first time)
 			require("barbecue.ui").toggle()
+			vim.notify("Zen Mode disabled", "info", { title = "Zen Mode" })
 		end,
 	},
 	config = function(_, opts)
 		local zen_mode = require("zen-mode")
 		zen_mode.setup(opts)
-		vim.keymap.set("n", "<leader>uz", "<cmd>ZenMode<CR>", { desc = "Toggle Zen Mode" })
+
+		vim.keymap.set("n", "<leader>uz", function()
+			zen_mode.toggle({
+				window = {
+					-- width = 0.85, -- width will be 85% of the editor width
+				},
+			})
+		end, { desc = "Toggle Zen Mode" })
 	end,
 }
