37735bb8d5349d3fb83adface7bb41279f48c074
“axzilla” <hello@axeladrian.com>
2024-05-22 17:09:40 +0200

    feat: wezterm, change theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,8 +1,23 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 20
+
 config.native_macos_fullscreen_mode = false
-config.font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = true })
+config.font = wezterm.font({
+	family = "JetBrains Mono",
+	-- family = "Fira Code",
+	-- family = "Monaspace Neon",
+	-- family = "Monaspace Argon",
+	-- family = "Monaspace Xenon",
+	-- family = "Monaspace Radon",
+	-- family = "Monaspace Krypton",
+	weight = "Medium",
+	-- italic = true,
+})
+
+config.window_padding = { bottom = 0 }
 config.hide_tab_bar_if_only_one_tab = true
 config.font_size = 14
 config.line_height = 1.2
