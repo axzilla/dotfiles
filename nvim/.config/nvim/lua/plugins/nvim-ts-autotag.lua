c5b7ea98149c960310f73ed0592c3ae97e620179
“axzilla” <hello@axeladrian.com>
2024-05-22 09:19:46 +0200

    feat: ts-autotag, use fixed branch

--- nvim/.config/nvim/lua/plugins/nvim-ts-autotag.lua
+++ nvim/.config/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -1,8 +1,6 @@
 ---@diagnostic disable: missing-parameter
 return {
-	"axzilla/nvim-ts-autotag",
-	branch = "fix/templ-auto-rename",
-	-- dir = "~/dev/nvim-ts-autotag",
+	"windwp/nvim-ts-autotag",
 	config = function()
 		local ts_autotag = require("nvim-ts-autotag")
 		ts_autotag.setup()
