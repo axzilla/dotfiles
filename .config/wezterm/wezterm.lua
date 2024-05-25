c3eee370690c20c8aedad23f43bb2051046bcc14
“axzilla” <hello@axeladrian.com>
2024-05-25 16:15:35 +0200

    feat: tmux UI

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,28 +1,18 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.window_background_opacity = 0.60
+config.window_background_opacity = 0.80
 config.macos_window_background_blur = 20
 config.color_scheme = "solarized_dark"
 
 config.native_macos_fullscreen_mode = false
 config.font = wezterm.font({
 	family = "JetBrains Mono",
-	-- family = "Fira Code",
-	-- family = "Monaspace Neon",
-	-- family = "Monaspace Argon",
-	-- family = "Monaspace Xenon",
-	-- family = "Monaspace Radon",
-	-- family = "Monaspace Krypton",
 	weight = "Medium",
-	-- italic = true,
 })
 --
 config.window_padding = {
-	-- top = 0,
-	-- right = 0,
 	bottom = 0,
-	-- left = 0,
 }
 
 config.hide_tab_bar_if_only_one_tab = true
