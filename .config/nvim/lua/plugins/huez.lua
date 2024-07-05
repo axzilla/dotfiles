778b4e449ef864ef9fa05e06b97eba1fbb50b8af
“axzilla” <hello@axeladrian.com>
2024-07-05 18:36:42 +0200

    feat: neovim, change some colorscheme related stuff

--- .config/nvim/lua/plugins/huez.lua
+++ .config/nvim/lua/plugins/huez.lua
@@ -1,59 +1,38 @@
--- ---@diagnostic disable: missing-fields
--- return {
--- 	"vague2k/huez.nvim",
--- 	dependencies = {
--- 		"nvim-telescope/telescope.nvim",
--- 		"stevearc/dressing.nvim",
--- 	},
--- 	config = function()
--- 		local huez = require("huez")
---
--- 		huez.setup({
--- 			omit = {
--- 				-- v9.x
--- 				"default",
--- 				"desert",
--- 				"evening",
--- 				"industry",
--- 				"koehler",
--- 				"morning",
--- 				"murphy",
--- 				"pablo",
--- 				"peachpuff",
--- 				"ron",
--- 				"shine",
--- 				"slate",
--- 				"torte",
--- 				"zellner",
--- 				"blue",
--- 				"darkblue",
--- 				"delek",
--- 				"quiet",
--- 				"elflord",
--- 				"habamax",
--- 				"lunaperche",
--- 				-- v10.x
--- 				"retrobox",
--- 				"sorbet",
--- 				"vim",
--- 				"wildcharm",
--- 				"zaibatsu",
--- 			},
--- 		})
---
--- 		local colorscheme = require("huez.api").get_colorscheme()
--- 		vim.cmd("colorscheme " .. colorscheme)
---
--- 		vim.keymap.set("n", "<leader>fC", "<cmd>Huez<CR>", { desc = "Find Colorscheme" })
--- 	end,
--- }
-
 return {
 	"vague2k/huez.nvim",
 	-- if you want registry related features, uncomment this
 	-- import = "huez-manager.import"
 	config = function()
-		require("huez").setup({})
+		require("huez").setup({
+			exclude = {
+				"desert",
+				"evening",
+				"industry",
+				"koehler",
+				"morning",
+				"murphy",
+				"pablo",
+				"peachpuff",
+				"ron",
+				"shine",
+				"slate",
+				"torte",
+				"zellner",
+				"blue",
+				"darkblue",
+				"delek",
+				"quiet",
+				"elflord",
+				"habamax",
+				"lunaperche",
+				"zaibatsu",
+				"wildcharm",
+				"sorbet",
+				"vim",
+				"default",
+				"retrobox",
+			},
+		})
 
 		local pickers = require("huez.pickers")
 
