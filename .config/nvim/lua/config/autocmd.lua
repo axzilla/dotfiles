7e4eeabdc0b85252f1061ad253e9e241272592ba
axzilla <chanter-acidic-0r@icloud.com>
2024-08-02 10:27:24 +0200

    feat: neovim, add wrap and linebreak for md files

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -5,3 +5,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
 	pattern = "*",
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.linebreak = true
+	end,
+})
