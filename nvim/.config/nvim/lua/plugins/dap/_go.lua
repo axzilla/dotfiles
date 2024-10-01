566384aa57a9e1dfda0bc41e290952f75622971b
axzilla <chanter-acidic-0r@icloud.com>
2024-10-01 16:45:42 +0200

    setup go debugger

--- nvim/.config/nvim/lua/plugins/dap/_go.lua
+++ nvim/.config/nvim/lua/plugins/dap/_go.lua
@@ -1,7 +1,21 @@
+-- In der Datei plugins/dap/_go.lua
+
 local M = {}
 
 M.setup = function()
+	local dap = require("dap")
 	require("dap-go").setup()
+
+	dap.configurations.go = {
+		{
+			type = "go",
+			name = "Debug Go Server with Templ and Air",
+			request = "launch",
+			program = "${workspaceFolder}/cmd/server",
+			args = { "serve", "--dir", "./pb_data" },
+			cwd = "${workspaceFolder}",
+		},
+	}
 end
 
 return M
