ae105ec741a89850857c52c1274ea65ae209219d
axzilla <hello@axeladrian.com>
2025-03-26 13:35:05 +0700

    updates

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -20,6 +20,11 @@ return {
 					},
 				},
 				panel = { enabled = false },
+				filetypes = {
+					-- because of RPC error in grug-far
+					["grug-far"] = false,
+					["grug-far-history"] = false,
+				},
 			})
 		end,
 	},
