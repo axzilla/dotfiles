b4ce24a9c815f6cc73e792fd483558a98c0571a3
axzilla <hello@axeladrian.com>
2025-06-22 15:10:11 +0700

    chore(neovim): add autoread

--- nvim/.config/nvim/lua/config/autocmd.lua
+++ nvim/.config/nvim/lua/config/autocmd.lua
@@ -1,26 +1,37 @@
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-	group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
-	pattern = "*",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+  pattern = "*",
 })
 
 -- Set wrap and linebreak
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "markdown", "txt" },
-	callback = function()
-		vim.opt_local.wrap = true
-		vim.opt_local.linebreak = true
-	end,
+  pattern = { "markdown", "txt" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+  end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "go", "templ" },
-	callback = function()
-		vim.bo.expandtab = false -- Go idiomatisch: TABS
-		vim.bo.tabstop = 4 -- Standard Go tab width
-		vim.bo.shiftwidth = 4
-	end,
+  pattern = { "go", "templ" },
+  callback = function()
+    vim.bo.expandtab = false -- Go idiomatisch: TABS
+    vim.bo.tabstop = 4       -- Standard Go tab width
+    vim.bo.shiftwidth = 4
+  end,
+})
+
+-- Automatically reload files when they change on disk // only works with `autoread = true` (see options.lua)
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+  command = "checktime",
+})
+-- Notify when a file is changed on disk
+im.api.nvim_create_autocmd("FileChangedShellPost", {
+  callback = function()
+    vim.notify("File changed on disk. Buffer reloaded!", vim.log.levels.INFO)
+  end,
 })
